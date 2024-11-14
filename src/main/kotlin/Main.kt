ab3c324d07cb3f7b2b27e7ec913033f3d2580f76
Asus <turgunboyevjurabek777@gmail.com>
2024-11-14 11:10:20 +0500

    MergeTwoSortedLists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
-import problems.ValidParentheses.Solution
-
+import problems.MergeTwoSortedLists.ListNode
+import problems.MergeTwoSortedLists.Solution
 
 fun main(args: Array<String>) {
-
     val solution = Solution()
 
+    val list1= ListNode(1)
+    val list2= ListNode(2)
 
-    println(solution.isValid("( [ Jurabek ] )"))
+    println(solution.mergeTwoLists(list1, list2))
 
 }
---
+++ src/main/kotlin/problems/ MergeTwoSortedLists/Solution.kt
@@ -0,0 +1,22 @@
+package problems.MergeTwoSortedLists
+class ListNode(var `val`: Int){
+    var next: ListNode?=null
+}
+
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+       if (list1 == null && list2 == null) return null
+
+        if (list1 == null) return list2
+
+        if ( list2 == null) return list1
+
+        if ( list1.`val` < list2.`val`) {
+            list1.next=mergeTwoLists(list1.next,list2)
+            return list1
+        }
+
+        list2.next = mergeTwoLists(list2.next, list1)
+        return list2
+    }
+}
